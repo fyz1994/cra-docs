84714eecf191573289d2de8ac68b543dae127f80
fyz1994 <yizhen.fan@baishancloud.com>
2019-12-02 11:47:41 +0800

    change: 加上评论的 OAuth APP 配置

--- docs/.vuepress/components/comment/comment.vue
+++ docs/.vuepress/components/comment/comment.vue
@@ -17,15 +17,15 @@ export default {
     body.appendChild(script);
     script.onload = () => {
       const commentConfig = {
-        clientID: "456079439c50848a6193",
-        clientSecret: "e2c42b4c023fa219beea2f67fd397b569fadfe08",
-        repo: "notes",
+        clientID: "06d8ac6a043dbf4e4b90",
+        clientSecret: "99f78d789eff94ddc5c59aa7485104be7f9e8f23",
+        repo: "cra-docs",
         owner: "fyz1994",
         // 这里接受一个数组，可以添加多个管理员，可以是你自己
         admin: ["fyz1994"],
         // id 用于当前页面的唯一标识，一般来讲 pathname 足够了，
         // 但是如果你的 pathname 超过 50 个字符，GitHub 将不会成功创建 issue，此情况可以考虑给每个页面生成 hash 值的方法.
-        id: document.title.replace(/\s\|\s去冲浪鸭|《|》/g, ""),
+        id: document.title.replace(/\s\|\sbs-cra|《|》/g, ""),
         distractionFreeMode: false
       };
       const gitalk = new Gitalk(commentConfig);
